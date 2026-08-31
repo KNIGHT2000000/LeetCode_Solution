5a51dbb4b1c97854fc90b85dda95cee4a7402bb0
vyomesh shukla <166438543+KNIGHT2000000@users.noreply.github.com>
2026-08-31 14:00:55 +0530

    [219] [Time Beats: 29.53%] [Memory Beats: 9.10%] - LeetPush

--- 219-Contains-Duplicate-II.java
+++ 219-Contains-Duplicate-II.java
@@ -1,3 +1,39 @@
+//bad approach--> defaeats the purpose of hashmap and o(n2)
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         //return 2 indices if element if the duplicate exists and abs()
+//            Map<Integer,Integer> mp =new HashMap<>();
+//      // the approach here is to loop throught the hashmap and 
+//      for(int i=0;i<nums.length;i++){
+//         mp.put(i,nums[i]);
+//      }  
+
+
+// for(int i=0;i<nums.length;i++){
+
+
+//      //now wehave to loop over ecah entry
+//      for(Map.Entry<Integer,Integer> entry :mp.entrySet() ){
+//         if(i==entry.getKey()){
+//             continue;
+//         }
+//         if(nums[i]==entry.getValue() && Math.abs(i-entry.getKey())<=k){
+//             return true;
+
+//         }
+//         else{
+//             continue;
+//         }
+
+
+
+        
+//      }    }
+
+//      return false;
+//     }
+// }
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         // we have to store value and index as key value pair remember phonbe book analogy 

35642f8897aa1c918c049e25049d395d4265df0c
vyomesh shukla <166438543+KNIGHT2000000@users.noreply.github.com>
2026-09-23 19:45:47 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 51.95%] - LeetPush

---
+++ 144-Binary-Tree-Preorder-Traversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void helper(TreeNode root,List<Integer> res)
+    {
+        //tree node helper function is used to get the
+        if(root==null){
+            return;
+        }
+        res.add(root.val);
+        helper(root.left,res);
+        helper(root.right,res);
+        
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        helper(root,res);
+        return res;
+        //
+        
+    }
+}
